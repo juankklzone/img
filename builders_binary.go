0c210472b9a1cf65da2c072df713f418044d83c4
John the ripper <juan@alepht.com>
2016-05-03 10:11:30 -0500

    Grayscale selector

--- builders_binary.go
+++ builders_binary.go
@@ -1,6 +1,9 @@
 package img
 
-import "sync"
+import (
+	"image"
+	"sync"
+)
 
 //NewBinaryImage returns a BinaryImage with img's size
 //if the color is greater than div, it's set up to true
@@ -20,7 +23,33 @@ func NewBinaryImage(img GrayImage, div uint8) BinaryImage {
 
 func readBinaryRow(wg *sync.WaitGroup, from GrayImage, to BinaryImage, x, maxY int, div uint8) {
 	for y := 0; y < maxY; y++ {
-		to[x][y] = from.AtGray(x, y) > div
+		to[x][y] = from.AtGray(x, y) < div
+	}
+	wg.Done()
+}
+
+//NewBinary builds a binary image from an image.Image
+func NewBinary(img image.Image, div uint8) BinaryImage {
+	borders := img.Bounds()
+	x, y := borders.Max.X, borders.Max.Y
+	bi := make(BinaryImage, x)
+	wg := new(sync.WaitGroup)
+	wg.Add(x)
+	for x := range bi {
+		bi[x] = make([]bool, y)
+		readBinary(wg, img, bi, x, y, div)
+	}
+	wg.Wait()
+	return bi
+}
+
+func readBinary(wg *sync.WaitGroup, from image.Image, to BinaryImage, x, maxY int, div uint8) {
+	for y := 0; y < maxY; y++ {
+		r, g, b, _ := from.ColorModel().Convert(from.At(x, y)).RGBA()
+		gray := uint8(0.299*float32(r) + 0.587*float32(g) + 0.114*float32(b))
+		if gray >= div {
+			to[x][y] = true
+		}
 	}
 	wg.Done()
 }
